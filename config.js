1b5d0b87cd6bca8488c3cfd5efaa70e9a2065718
abdullahthamer90 <55867119+abdullahthamer90@users.noreply.github.com>
2022-08-31 05:57:15 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tokens: [
+    "MTAwNzg0NjA1OTU4NzkzNjI4Nw.GsqnTJ.wzf7MuYItq63DpgM4K4ChVAvxjIrzC8ufJReRY",
+    "MTAxMTQxODQ2MDEyNDg4NTAzMg.GzTVXE.KzXNWB7wToAr64voWkD473gaeycqB3t0nVgTrE",
+    "MTAxMTQxODgwNTc2MDY5MjI4NQ.GmWr3W.vxSwN-vIuqQUzweqtLcnDATUhkWRVr2XIr7Lvw",
+    "token",
+  ], ///توكنات حسابات الترحيب
+  voice_channel: "1014362077009301606", ///ايدي الروم اللي بدك يثبت فيه
+  text_channel: "912094572912447488", ///ايدي روم اللي بدك يرحب فيه
+  your_server: "850517189474910248", /// ايدي السيرفر تبعك
+};
